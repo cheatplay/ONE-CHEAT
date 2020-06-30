b312f6d36df969d4d4f6a7b10d75c4586d77c053
cheatplay <67628126+cheatplay@users.noreply.github.com>
2020-06-30 21:59:04 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,6 +1,6 @@
 <?php
-$link = mysqli_connect("localhost", "mysql_username","mysql_password");
-$database = mysqli_select_db($link, "mysql_database");
+$link = mysqli_connect("localhost", "cheatplay3019","a654a123");
+$database = mysqli_select_db($link, "cheatplay3019");
 
 $user = $_GET['username'];
 $password = $_GET['password'];
